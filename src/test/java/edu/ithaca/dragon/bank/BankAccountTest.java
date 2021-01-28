dd72f2c8a40d6c91b3f052f36120068b7013e4ad
Bobolicious cool kid <bbradley@ithaca.edu>
2021-01-28 15:14:26 -0500

    new tests

--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -8,19 +8,47 @@ class BankAccountTest {
 
     @Test
     void getBalanceTest() {
+        //Initial Test
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
-
         assertEquals(200, bankAccount.getBalance());
+
+        //High balance
+        BankAccount bankAccount2 = new BankAccount("a@b.com", 2000);
+        assertEquals(2000, bankAccount2.getBalance());
+
+        //Low balance
+        BankAccount bankAccount3 = new BankAccount("a@b.com", 2);
+        assertEquals(2, bankAccount3.getBalance());
+
+        //Negative balance
+        BankAccount bankAccount4 = new BankAccount("a@b.com", -20);
+        assertEquals(-20, bankAccount4.getBalance());
+
     }
 
     @Test
     void withdrawTest() throws InsufficientFundsException{
+        //first initial tests
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         bankAccount.withdraw(100);
-
         assertEquals(100, bankAccount.getBalance());
+
+        //Metod should throw an exception for when the withdrawal is greater than account balance
         assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300));
+
+        //Test for an empty withdrawal
+        BankAccount bankAccount2 = new BankAccount("a@b.com", 300);
+        bankAccount2.withdraw(0);
+        assertEquals(300, bankAccount2.getBalance());
+        
+        //Negative Numbered withdrawal 
+        bankAccount2.withdraw(-10);
+        assertEquals(300, bankAccount2.getBalance());
+
+        
+
     }
+        
 
     @Test
     void isEmailValidTest(){
@@ -33,7 +61,7 @@ class BankAccountTest {
         assertFalse(BankAccount.isEmailValid("@ab."));
         assertFalse(BankAccount.isEmailValid(".@ab"));
 
-        
+
         assertFalse(BankAccount.isEmailValid("gmail"));
         assertFalse(BankAccount.isEmailValid("@a"));
         assertFalse(BankAccount.isEmailValid(".a"));
